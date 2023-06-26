cb0d561503bee1a37e10efd4a8a85110855b54f7
Pavel Viarbitski <verbipo@gmail.com>
2023-06-26 17:36:32 +0200

    feat: os functions

---
+++ src/helpers/os.js
@@ -0,0 +1,57 @@
+import { errorHandler } from './errorHandler.js';
+import os from 'os';
+
+export async function osHeandler(args) {
+  try {
+    switch (args) {
+      case '--EOL':
+        printEndOfLine();
+        break;
+      case '--cpus':
+        printCpuInfo();
+        break;
+      case '--homedir':
+        printHomeDirectory();
+        break;
+      case '--username':
+        printCurrentUsername();
+        break;
+      case '--architecture':
+        printCpuArchitecture();
+        break;
+      default:
+        console.log(`Invalid input`);
+        break;
+    }
+  } catch (error) {
+    errorHandler(error);
+  }
+}
+
+function printEndOfLine() {
+  console.log(`End-Of-Line: ${JSON.stringify(os.EOL)} \n`);
+}
+
+function printCpuInfo() {
+  const cpus = os.cpus().map((cpu) => ({
+    Model: cpu.model,
+    'Clock rate': `${(cpu.speed / 1000).toFixed(2)} GHz \n`,
+  }));
+  console.log(`CPUs count: ${cpus.length}`);
+  console.table(cpus);
+}
+
+function printHomeDirectory() {
+  const homeDir = os.homedir();
+  console.log(`Home directory: ${homeDir} \n`);
+}
+
+function printCurrentUsername() {
+  const username = os.userInfo().username;
+  console.log(`Current user name: ${username} \n`);
+}
+
+function printCpuArchitecture() {
+  const architecture = os.arch();
+  console.log(`CPU architecture: ${architecture} \n`);
+}
